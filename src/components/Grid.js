bfec581d951d63e14ed8060d4d0255adcf3b2485
Jana Scheuble <janascheuble@Janas-MacBook-Air.local>
2020-07-28 11:24:40 -0700

    cell toggle alive/dead

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -5,15 +5,19 @@ const colCount = 25
 
 const Grid = () => {
 
+    // initialize grid with zeros
     const [grid, setGrid] = useState(Array(rowCount).fill(Array(colCount).fill(0)))
-
-    console.log('grid', grid)
 
     return (
         <div style={{ display: 'grid', gridTemplateColumns: `repeat(${colCount}, 15px)` }} >
             {grid.map((row, i) =>
                 row.map((col, j) => {
-                    return <div key={`${i}-${j}`} className='cell' style={{ backgroundColor: grid[i][j] ? 'black' : undefined }} />
+                    return <div key={`${i}-${j}`} onClick={() => {
+                        let gridCopy = JSON.parse(JSON.stringify(grid))
+                        // update this, toggle must first check if the game is running
+                        gridCopy[i][j] = gridCopy[i][j] ? 0 : 1;
+                        setGrid(gridCopy)
+                    }} className='cell' style={{ backgroundColor: grid[i][j] ? 'black' : undefined }} />
                 }))}
         </div>
     )
