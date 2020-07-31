ff8b5977110e634041ef9dd5315bc8cd2543d31e
Jana Scheuble <janascheuble@Janas-MacBook-Air.local>
2020-07-30 18:57:20 -0700

    about popup

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import styled from 'styled-components'
 
-const About = () => {
+const TitleWrapper = styled.div`
+    display: flex;
+    justify-content: space-between;
+`;
+
+const About = props => {
     return (
-        <div className='popup'>
-            about
+        <div className='popup-container'>
+            <div className='popup'>
+                <TitleWrapper>
+                    <h4>About The Game of Life</h4>
+                    <span onClick={() => props.setAbout(false)}>&#x2612;</span>
+                </TitleWrapper>
+            </div>
         </div>
     )
 }
